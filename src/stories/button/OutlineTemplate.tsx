c30e9fcf783e0cb12fe6c35ce2bd07c0c660de2e
Larisa Markova <lovemirror@rambler.ru>
2025-10-09 12:15:32 +0300

    Delete redundant stories

--- src/stories/button/OutlineTemplate.tsx
+++
@@ -1,5 +0,0 @@
-import { ChartButton, type ChartButtonProps } from "./ChartButton";
-
-export const OutlineTemplate = (props: ChartButtonProps) => (
-  <ChartButton {...props} appearance="secondary" />
-);
--- src/stories/button/WithIconTemplate.tsx
+++
@@ -1,6 +0,0 @@
-import { ChartButton, type ChartButtonProps } from "./ChartButton";
-import { CategoryHeartOutline } from "@admiral-ds/icons";
-
-export const WithIconTemplate = (props: ChartButtonProps) => (
-  <ChartButton {...props} iconStart={<CategoryHeartOutline />} />
-);
